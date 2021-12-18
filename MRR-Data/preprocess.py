650793f512ec54018b16b07bb5676cc2f6527543
ChrisGtmhub <christopher.odell@gtmhub.com>
2021-12-17 19:01:43 -0700

    update

---
+++ MRR-Data/preprocess.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+import pandas as pd
+from pathlib import Path
+
+home = str(Path.home())
+
+de_path = os.path.join(home,"OneDrive - Gtmhub Ltd", "Analytics", "Data Engineering")
+
+df = pd.read_csv(os.path.join(de_path, "CustomerMRRChange2021.csv"))
+
+
+def main():
+
+    for i, r in df.iterrows():
+        print(i, r)
+        sys.exit()
+
+
+
+if __name__ == '__main__':
+
+    main()
