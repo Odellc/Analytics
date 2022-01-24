aeafb3341c4547a29446ca2c074a3bbee99609a0
ChrisGtmhub <christopher.odell@gtmhub.com>
2022-01-24 16:35:32 -0700

    update adf

---
+++ Data Preprocessing Python/data_processing_tools.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+import matplotlib.pylot as plt
+import pandas as od
+
+
+THIS_DIRECTORY_PATH = os.path.dirname(os.path.abspath(__file__))
+ABS_PATH = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2])
+print(ABS_PATH)
+
+data_path = os.path.join(os.curdir, )
+
+dataset = pd.read_csv()
+
+
+
+
+if __name__ == "__main__":
+    main()
