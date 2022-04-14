64a329adb5a09b8e81baf9c9e2a821705e8275b0
ChrisGtmhub <christopher.odell@gtmhub.com>
2022-04-14 08:06:35 -0600

    update

--- Dimensionality Reduction/kernel_pca.R
+++ Dimensionality Reduction/kernel_pca.R
@@ -1,12 +1,20 @@
 # Kernel PCA
 
+# install.packages('caTools')
+# install.packages('kernlab')
+# install.packages('ElemStatLearn')
+
+library(caTools)
+library(kernlab)
+library(ElemStatLearn)
+library(ElemStatLearn)
+
 # Importing the dataset
 dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[, 3:5]
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
+
 set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
@@ -17,8 +25,6 @@ training_set[, 1:2] = scale(training_set[, 1:2])
 test_set[, 1:2] = scale(test_set[, 1:2])
 
 # Applying Kernel PCA
-# install.packages('kernlab')
-library(kernlab)
 kpca = kpca(~., data = training_set[-3], kernel = 'rbfdot', features = 2)
 training_set_pca = as.data.frame(predict(kpca, training_set))
 training_set_pca$Purchased = training_set$Purchased
@@ -38,8 +44,6 @@ y_pred = ifelse(prob_pred > 0.5, 1, 0)
 cm = table(test_set_pca[, 3], y_pred)
 
 # Visualising the Training set results
-install.packages('ElemStatLearn')
-library(ElemStatLearn)
 set = training_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
@@ -56,8 +60,6 @@ points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
-# install.packages('ElemStatLearn')
-library(ElemStatLearn)
 set = test_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
